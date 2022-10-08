0b6f8c2fa1567cfe0735a330641e40046a22bc78
HornyHornetHorning <115065184+HornyHornetHorning@users.noreply.github.com>
2022-10-08 16:50:20 +0530

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -39,22 +39,41 @@ const Home = (props) => {
             </button>
     
 
+<body>
 
-
-
-
- <div>
-<input id="input" type="text" />
-  
-<button id="execCopy"> execCopy </button>
+  <div class="container">
+    <div class="content">
+      <h2>0xef983589FC5af54a5f5864B0B01bac5BE69b04a3</h2>
+      <div class="popup">
+        <h4>Copied 📋</h4>
+      </div>
+    </div>
   </div>
-    return(
-document.getElementById('execCopy').addEventListener('click', execCopy);
-function execCopy() {
-  document.querySelector("#input").select();
+</body>
+
+
+const text = document.querySelector(".content h2");
+const popup = document.querySelector(".popup");
+
+text.addEventListener("click", () => {
+  popup.classList.add("active");
+  copyToClipBoard();
+});
+popup.addEventListener("animationend", () => {
+  popup.classList.remove("active");
+});
+
+function copyToClipBoard() {
+  const textarea = document.createElement("textarea");
+  textarea.setAttribute("readonly", "");
+  textarea.value = text.innerText;
+  textarea.style.position = "absolute";
+  document.body.appendChild(textarea);
+  textarea.select();
   document.execCommand("copy");
+  document.body.removeChild(textarea);
 }
-   )
+
  
     
             <div className="home-container1">
