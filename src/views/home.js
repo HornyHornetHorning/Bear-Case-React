87071daaa44513f960f7b1bacc0a7f143c859002
HornyHornetHorning <115065184+HornyHornetHorning@users.noreply.github.com>
2022-10-08 17:02:41 +0530

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -4,17 +4,6 @@ import { Helmet } from 'react-helmet'
 
 import './home.css'
 
-<body>
-
-  <div class="container">
-    <div class="content">
-      <h2>0xef983589FC5af54a5f5864B0B01bac5BE69b04a3</h2>
-      <div class="popup">
-        <h4>Copied 📋</h4>
-      </div>
-    </div>
-  </div>
-</body>
 
 
 const text = document.querySelector(".content h2");
@@ -77,6 +66,17 @@ const Home = (props) => {
             </button>
     
 
+<body>
+
+  <div class="container">
+    <div class="content">
+      <h2>0xef983589FC5af54a5f5864B0B01bac5BE69b04a3</h2>
+      <div class="popup">
+        <h4>Copied 📋</h4>
+      </div>
+    </div>
+  </div>
+</body>
 
  
     
