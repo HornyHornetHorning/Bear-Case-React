28e1347ecb818630b399428a5c52e45b760c2506
HornyHornetHorning <115065184+HornyHornetHorning@users.noreply.github.com>
2022-10-08 17:05:32 +0530

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -6,30 +6,6 @@ import './home.css'
 
 
 
-const text = document.querySelector(".content h2");
-const popup = document.querySelector(".popup");
-
-text.addEventListener("click", () => {
-  popup.classList.add("active");
-});
-  text.addEventListener("click", () => {
-  copyToClipBoard();
-});
-popup.addEventListener("animationend", () => {
-  popup.classList.remove("active");
-});
-
-function copyToClipBoard() {
-  const textarea = document.createElement("textarea");
-  textarea.setAttribute("readonly", "");
-  textarea.value = text.innerText;
-  textarea.style.position = "absolute";
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textarea);
-}
-
 
 const Home = (props) => {
   return (
@@ -78,6 +54,31 @@ const Home = (props) => {
   </div>
 </body>
 
+    return(
+const text = document.querySelector(".content h2");
+const popup = document.querySelector(".popup");
+
+text.addEventListener("click", () => {
+  popup.classList.add("active");
+});
+  text.addEventListener("click", () => {
+  copyToClipBoard();
+});
+popup.addEventListener("animationend", () => {
+  popup.classList.remove("active");
+});
+
+function copyToClipBoard() {
+  const textarea = document.createElement("textarea");
+  textarea.setAttribute("readonly", "");
+  textarea.value = text.innerText;
+  textarea.style.position = "absolute";
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+}
+)
  
     
             <div className="home-container1">
